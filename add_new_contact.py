07bfa2638f7a592cf93ad5b49662f25a9108405d
Anna <anna.inboxx@gmail.com>
2020-03-30 18:34:28 +0300

    add auxiliary class

--- add_new_contact.py
+++ add_new_contact.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from contact import Contact
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
@@ -16,7 +17,7 @@ class AddNewContact(unittest.TestCase):
         driver = self.driver
         self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
-        self.create_new_contact(driver)
+        self.create_new_contact(driver, Contact(name="Tom", middlename="John",lastname= "Smith", homephone="89337774448", email="djdjk@dljkk.ru"))
         self.return_to_edit_entry_page(driver)
         self.logout(driver)
 
@@ -26,34 +27,23 @@ class AddNewContact(unittest.TestCase):
     def return_to_edit_entry_page(self, driver):
         driver.find_element_by_link_text("home page").click()
 
-    def create_new_contact(self, driver, name="Tom", middlename="John", lastname="Smith", homephone="89337774448",
-                           email="djdjk@dljkk.ru"):
+    def create_new_contact(self, driver, contact):
         # init new contact creation
         driver.find_element_by_link_text("add new").click()
         # fill in contact data
         driver.find_element_by_name("firstname").click()
         driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(name)
+        driver.find_element_by_name("firstname").send_keys(contact.name)
         driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(middlename)
+        driver.find_element_by_name("middlename").send_keys(contact.middlename)
         driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(lastname)
+        driver.find_element_by_name("lastname").send_keys(contact.lastname)
         driver.find_element_by_name("home").click()
         driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(homephone)
+        driver.find_element_by_name("home").send_keys(contact.homephone)
         driver.find_element_by_name("email").click()
         driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(email)
-        driver.find_element_by_name("bday").click()
-        Select(driver.find_element_by_name("bday")).select_by_visible_text("17")
-        driver.find_element_by_name("bday").click()
-        driver.find_element_by_name("homepage").click()
-        driver.find_element_by_name("bmonth").click()
-        Select(driver.find_element_by_name("bmonth")).select_by_visible_text("April")
-        driver.find_element_by_name("bmonth").click()
-        driver.find_element_by_name("byear").click()
-        driver.find_element_by_name("byear").clear()
-        driver.find_element_by_name("byear").send_keys("1999")
+        driver.find_element_by_name("email").send_keys(contact.email)
         # submit new contact creation
         driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
