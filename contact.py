eceac3e5cbc3b41f3af0db9c26888819b74d2e2e
Anna <anna.inboxx@gmail.com>
2020-03-30 18:34:36 +0300

    add auxiliary class

---
+++ contact.py
@@ -0,0 +1,8 @@
+class Contact:
+
+    def __init__(self, name, middlename, lastname, homephone, email):
+        self.name = name
+        self.middlename = middlename
+        self.lastname = lastname
+        self.homephone = homephone
+        self.email = email
