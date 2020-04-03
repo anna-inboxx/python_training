302e99b1980a6ce27109818c5988c2e3ee64e9a6
Anna <anna.inboxx@gmail.com>
2020-04-03 20:32:18 +0300

    change to wd

--- add_new_contact.py
+++ add_new_contact.py
@@ -10,8 +10,8 @@ from contact import Contact
 
 class AddNewContact(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
     
     def test_add_new_contact(self):
         self.open_home_page()
@@ -21,60 +21,60 @@ class AddNewContact(unittest.TestCase):
         self.logout()
 
     def logout(self):
-        driver = self.driver
-        driver.find_element_by_link_text("Logout").click()
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
 
     def return_to_edit_entry_page(self):
-        driver = self.driver
-        driver.find_element_by_link_text("home page").click()
+        wd = self.wd
+        wd.find_element_by_link_text("home page").click()
 
     def create_new_contact(self, contact):
-        driver = self.driver
+        wd = self.wd
         # init new contact creation
-        driver.find_element_by_link_text("add new").click()
+        wd.find_element_by_link_text("add new").click()
         # fill in contact data
-        driver.find_element_by_name("firstname").click()
-        driver.find_element_by_name("firstname").clear()
-        driver.find_element_by_name("firstname").send_keys(contact.name)
-        driver.find_element_by_name("middlename").clear()
-        driver.find_element_by_name("middlename").send_keys(contact.middlename)
-        driver.find_element_by_name("lastname").clear()
-        driver.find_element_by_name("lastname").send_keys(contact.lastname)
-        driver.find_element_by_name("home").click()
-        driver.find_element_by_name("home").clear()
-        driver.find_element_by_name("home").send_keys(contact.homephone)
-        driver.find_element_by_name("email").click()
-        driver.find_element_by_name("email").clear()
-        driver.find_element_by_name("email").send_keys(contact.email)
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.name)
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.homephone)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
         # submit new contact creation
-        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+        wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
     def login(self, username, password):
-        driver = self.driver
-        driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys(username)
-        driver.find_element_by_name("pass").click()
-        driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys(password)
-        driver.find_element_by_xpath("//input[@value='Login']").click()
+        wd = self.wd
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self):
-        driver = self.driver
-        driver.get("https://localhost/addressbook/index.php")
+        wd = self.wd
+        wd.get("https://localhost/addressbook/index.php")
 
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
+        try: self.wd.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
         return True
     
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
+        try: self.wd.switch_to_alert()
         except NoAlertPresentException as e: return False
         return True
     
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to_alert()
+            alert = self.wd.switch_to_alert()
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
