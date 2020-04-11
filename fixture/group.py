c965d13baef963f4a9e3c36dfb2a685d5680d48a
Anna <anna.inboxx@gmail.com>
2020-04-12 00:11:02 +0300

    created conftest

--- fixture/group.py
+++ fixture/group.py
@@ -10,6 +10,7 @@ class GroupHelper:
 
     def create(self, group):
         wd = self.app.wd
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -22,6 +23,16 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_group_page()
 
     def open_groups_page(self):
         wd = self.app.wd
