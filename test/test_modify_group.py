645692153d92381233cdfd95fb2a92b518686cbb
anna-inboxx <agness2B!>
2020-04-16 00:34:23 +0300

    test modify group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,9 +2,11 @@
 from model.group import Group
 
 def test_modify_group_name(app):
-    app.group.modify_first_group(Group(name="New name"))
-    app.session.logout()
+    if app.group.count() == 0:
+        app.group.create(Group(name="testdel", header="test", footer="test"))
+        app.group.modify_first_group(Group(name="New name"))
 
 def test_modify_group_header(app):
-    app.group.modify_first_group(Group(header="New header"))
-    app.session.logout()
+    if app.group.count() == 0:
+        app.group.create(Group(name="testdel", header="test", footer="test"))
+        app.group.modify_first_group(Group(header="New header"))
