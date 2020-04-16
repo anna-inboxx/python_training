da15c3658971f1294594f141b20f67d1b8c161e5
anna-inboxx <agness2B!>
2020-04-16 19:44:39 +0300

    transition modification open_group

--- fixture/group.py
+++ fixture/group.py
@@ -23,6 +23,7 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_group_page()
 
     def edit_first_group(self):
         wd = self.app.wd
@@ -55,7 +56,8 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def fill_group_form(self,group):
         wd = self.app.wd
