27732efe348f243cad68aab09683c136e2808fbf
anna-inboxx <agness2B!>
2020-04-16 18:54:35 +0300

    modify contact 2

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,6 +1,5 @@
 from model.contact import Contact
 
-
 def test_del_contact(app):
     if app.contact.count() == 0:
         app.contact.create_new_contact(Contact(name="Tim", middlename="John",lastname= "Smuth", homephone="89337774448", email="djdjk@dljkk.ru"))
