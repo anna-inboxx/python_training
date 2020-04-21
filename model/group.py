758514a765ccc624b340b6142a20fb6b31eaeb51
Anna <anna.inboxx@gmail.com>
2020-04-22 01:15:13 +0300

    проверка равенства для test_del_group.py

--- model/group.py
+++ model/group.py
@@ -5,3 +5,10 @@ class Group:
         self.header = header
         self.footer = footer
         self.id = id
+
+    # функция, кот опеределяет как будет выглядеть наш объект при выводе на консоль
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
--- test/test_del_group.py
+++ test/test_del_group.py
@@ -8,3 +8,7 @@ def test_delete_first_group(app):
     app.group.delete_first_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
+    #из старого списка удаляем первую группу. Удаляем все элементы с 0 по 1, т.е. только с индексом 0
+    old_groups[0:1] = []
+    #сравниваем списки
+    assert old_groups == new_groups
