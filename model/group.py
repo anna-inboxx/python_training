367b04fab90c96112dd28601cd2e08546725cca2
Anna <anna.inboxx@gmail.com>
2020-04-23 17:46:38 +0300

    сортировка списков test_del_group.py

--- model/group.py
+++ model/group.py
@@ -1,3 +1,6 @@
+from sys import maxsize
+
+
 class Group:
 
     def __init__(self, name=None, header=None, footer=None, id=None):
@@ -6,9 +9,18 @@ class Group:
         self.footer = footer
         self.id = id
 
+    def __eq__(self, other):
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+
     # функция, кот опеределяет как будет выглядеть наш объект при выводе на консоль
     def __repr__(self):
         return "%s:%s" % (self.id, self.name)
 
-    def __eq__(self, other):
-        return self.id == other.id and self.name == other.name
+    #пишем правило (ключ для сравнения) если присвоено id, то берем его, если None, то maxsize
+    def id_or_max(self):
+        if self.id:
+            return int(self.id)
+        else:
+            return maxsize
+
+
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,18 +2,25 @@
 from model.group import Group
 
 def test_add_group(app):
-    # сохраняем старый список
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="test", header="test", footer="test"))
-    # создаем новый список
+    # помещаем в локальную переменную
+    group = Group(name="test", header="test", footer="test")
+    # передаем в метод create
+    app.group.create(group)
     new_groups = app.group.get_group_list()
-    #делаем проверку теста - тест работает если длина нового списка равна старый список +1
     assert len(old_groups) + 1 == len(new_groups)
+    # те же группы передаем в старый список. Получается проверка
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 
