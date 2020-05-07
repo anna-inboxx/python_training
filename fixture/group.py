bd67efb6b5684bb73f35df8ff3616849727d85fb
anna-inboxx <agness2B!>
2020-05-07 20:38:42 +0300

    случайность в изменении групп

--- fixture/group.py
+++ fixture/group.py
@@ -71,10 +71,13 @@ class GroupHelper:
             wd.find_element_by_name(field_name).clear()
             wd.find_element_by_name(field_name).send_keys(text)
 
-    def modify_first_group(self,new_group_data):
+    def modify_first_group(self):
+        self.modify_group_by_index(0)
+
+    def modify_group_by_index(self,index,new_group_data):
         wd = self.app.wd
         self.open_groups_page()
-        self.select_first_group()
+        self.select_group_by_index(index)
         # click edit
         wd.find_element_by_name("edit").click()
         self.fill_group_form(new_group_data)
@@ -82,6 +85,7 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
         self.group_cache = None
+
 
     def count(self):
         wd = self.app.wd
