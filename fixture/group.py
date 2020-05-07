5d74bc8abdfa983bb0f3d3ccc3473984e1eb47d2
anna-inboxx <agness2B!>
2020-05-07 20:20:55 +0300

    случайность в удалении групп

--- fixture/group.py
+++ fixture/group.py
@@ -30,14 +30,24 @@ class GroupHelper:
 
 
     def delete_first_group(self):
+       self.delete_group_by_index(0)
+
+    def delete_group_by_index(self,index):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_group_by_index(index)
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
         self.group_cache = None
+
+    def select_group_by_index(self,index):
+        wd = self.app.wd
+        wd.find_elements_by_name("selected[]")[index].click()
+
+    def select_first_group(self):
+        wd = self.app.wd
+        self.select_group_by_index(0)
 
     def open_groups_page(self):
         wd = self.app.wd
