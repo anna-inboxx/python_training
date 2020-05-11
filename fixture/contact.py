dc9181ae1e41076d5838ff2b8958b0fb1a639638
anna-inboxx <agness2B!>
2020-05-11 16:22:50 +0300

    13 task test_modify_contacts updated

--- fixture/contact.py
+++ fixture/contact.py
@@ -73,8 +73,7 @@ class ContactHelper:
 
     def modify_contact_by_index(self,index, new_contact_data):
          wd = self.app.wd
-         self.select_contact_by_index(index)
-         wd.find_element_by_xpath("//img[@alt='Edit']").click()
+         wd.find_elements_by_xpath("//img[@alt='Edit']")[index].click()
             # delete and put new values
          self.fill_contact_form(new_contact_data)
             # click update
