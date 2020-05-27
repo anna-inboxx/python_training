2b0e2388066f6a1608fe53363645fa4adbee8f2f
anna-inboxx <agness2B!>
2020-05-27 23:25:30 +0300

    CI

--- check_db_connection.py
+++ check_db_connection.py
@@ -5,25 +5,19 @@ from model.contact import Contact
 from model.group import Group
 
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 try:
-    contacts = db.get_contact_list()
+    contacts = db.get_contacts_in_group(Group(id="203"))
     for contact in contacts:
         print(contact)
     print(len(contacts))
 finally:
-    db.destroy()
-
-
-
-db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
-try:
-    l = db.get_contacts_in_group(Group(id="203"))
-    for item in l:
-        print(item)
-    print(len(l))
-finally:
     pass#db.destroy()
+
+
+
+
+#    l = db.get_contacts_in_group(Group(id="203"))
 
 
 
---
+++ testing.py
@@ -0,0 +1,7 @@
+rows = []
+cells = [2,3,5]
+for cell in cells:
+    rows.append(cell*2)
+print(rows)
+
+
